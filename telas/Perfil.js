f9f779cd715f4f8a73d611953f15bd01ba463fe7
megadogalo <mylene.jpeg@gmail.com>
2024-06-20 21:16:54 -0300

    Tela de perifl

--- telas/Perfil.js
+++ telas/Perfil.js
@@ -1,10 +1,32 @@
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet, Image } from 'react-native'
 import React from 'react'
 
 export default function Perfil() {
     return (
-    <View>
-        <Text>perfil</Text>
+    <View style={styles.container} >
+        <Image style={styles.profilePicture} source={require('../assets/profile-picture.png')} />
+
+        <Text style={styles.text }> Mylene Cavalcanti </Text>
     </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f0f0f0'
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#333'
+    },
+    profilePicture: {
+        height: 200,
+        width: 200,
+        borderRadius: 100,
+        marginBottom: 40
+    }
+});
